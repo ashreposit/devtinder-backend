8077b8f826d5bf11d0ca73505c0ef27c1866b866
ashreposit <anithaasha12@gmail.com>
2024-10-11 15:50:48 +0530

    added user model file and craete user api

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
 const {connection} = require("./config/database");
+const User = require("./model/user");
+
+// new create request
+app.post("/signup",async(req,res)=>{
+    
+    // create a new instance of User model
+    const user=new User({
+        firstName:"Asha",
+        lastName:"Laxmi",
+        emailId:"asha@gmail.com",
+        password:12345
+    });
+
+    try{
+        // save the data 
+        await user.save();
+        res.send("user added successfully");
+    }
+    catch(err){
+        res.status(400).send("error in creating user"+err.message);
+    }
+});
 
 connection().then(() => {
     console.log("successfully connected to the database...");
--- model/user.js
+++ model/user.js
@@ -3,22 +3,34 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
     firstName: {
-        type: String
+        type: String,
+        required:true
     },
     lastName: {
-        type: String
+        type: String,
+        required:true
     },
     emailId: {
-        type: String
+        type: String,
+        required:true
     },
     password: {
-        type: String
+        type: String,
+        required:true
     },
     age: {
         type: Number
     },
     gender: {
         type: String
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    modifiedAt:{
+        type:Date,
+        default:Date.now
     }
 });
 
